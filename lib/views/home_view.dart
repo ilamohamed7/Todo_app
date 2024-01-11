965788c6a7ed2f3af6802b1ca0a0277d3a47d768
Ali Algazar <ilamohamed7@gmail.com>
2024-01-11 07:57:38 +0200

    create appber at home view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -10,6 +10,33 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Notes',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Container(
+              height: 50,
+              width: 40,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade800,
+                borderRadius: BorderRadius.circular(13),
+              ),
+              child: Icon(
+                Icons.search,
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
